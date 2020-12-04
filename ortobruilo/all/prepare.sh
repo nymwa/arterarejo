9ffe562f4ac3a585fc8e8a2b4e1c948ec37d9e53
nymwa <nymwa0@gmail.com>
2020-12-04 22:21:47 +0900

    ortobruilo

---
+++ ortobruilo/all/prepare.sh
@@ -0,0 +1,14 @@
+set -ex
+
+if [ -z $WORKDIR ] ; then
+    WORKDIR=`dirname $0`
+fi
+cd $WORKDIR
+
+if [ -n $SGE_QSUB ] ; then
+    . /etc/profile.d/modules.sh
+    source ../../start.sh
+fi
+
+ortobruilo-prepare ../../afiksilo/all/frequency.tsv orto_dict.pickle --min-freq 500
+
