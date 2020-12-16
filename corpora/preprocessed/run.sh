e8860165d801f40340e007e3cdf70edde5ec8f91
nymwa <nymwa0@gmail.com>
2020-12-16 21:17:34 +0900

    run.sh for preprocessing lang

---
+++ corpora/preprocessed/run.sh
@@ -0,0 +1,17 @@
+set -ex
+
+if [ -z $WORKDIR ] ; then
+    WORKDIR=`dirname $0`
+fi
+cd $WORKDIR
+
+if [ -n $SGE_QSUB ] ; then
+    . /etc/profile.d/modules.sh
+    source ../../start.sh
+fi
+
+cat ../bea19/train/lang8/lang8.train.auto.bea19.m2 | m2_to_src > lang8.src
+cat ../bea19/train/lang8/lang8.train.auto.bea19.m2 | m2_to_trg > lang8.trg
+remove_identical lang8.src lang8.trg lang8.identical_removed.src lang8.identical_removed.trg
+paste lang8.identical_removed.src lang8.identical_removed.trg > lang8.identical_removed.tsv
+
