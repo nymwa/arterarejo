fb47eeef61243fd5ea6509eb64831c579a0e3c7d
nymwa <nymwa0@gmail.com>
2020-12-18 19:24:21 +0900

    modify eval config

---
+++ expt/absolute.sh
@@ -0,0 +1,2 @@
+fa_convert_eval_config eval_config.bea_train_16000.yaml eval_config.bea_train_16000.absolute.yaml
+fa_convert_eval_config eval_config.crawl19_16000.yaml eval_config.crawl19_16000.absolute.yaml
