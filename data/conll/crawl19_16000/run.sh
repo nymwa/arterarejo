d05536ab681db057c4aa0fec54499d8bd8d502e5
nymwa <nymwa0@gmail.com>
2021-02-12 23:35:25 +0900

    fix alt/noalt

--- data/conll/crawl19_16000/run.sh
+++ data/conll/crawl19_16000/run.sh
@@ -10,7 +10,7 @@ if [ -n $SGE_QSUB ] ; then
     source ../../../start.sh
 fi
 
-cat ../../../corpora/conll/conll13/release2.3.1/revised/data/official-preprocessed.m2 | m2_to_src | reguligilo -a | pyspm_encode --model_file ../../../bpe/crawl19_16000/crawl19.16000.model > valid.src
-cat ../../../corpora/conll/conll13/release2.3.1/revised/data/official-preprocessed.m2 | m2_to_trg | reguligilo -a | pyspm_encode --model_file ../../../bpe/crawl19_16000/crawl19.16000.model > valid.trg
-cat ../../../corpora/conll/conll14/conll14st-test-data/alt/official-2014.combined-withalt.m2 | m2_to_src | reguligilo -a | pyspm_encode --model_file ../../../bpe/crawl19_16000/crawl19.16000.model > test.src
+cat ../../../corpora/conll/conll13/release2.3.1/original/data/official-preprocessed.m2 | m2_to_src | reguligilo -a | pyspm_encode --model_file ../../../bpe/crawl19_16000/crawl19.16000.model > valid.src
+cat ../../../corpora/conll/conll13/release2.3.1/original/data/official-preprocessed.m2 | m2_to_trg | reguligilo -a | pyspm_encode --model_file ../../../bpe/crawl19_16000/crawl19.16000.model > valid.trg
+cat ../../../corpora/conll/conll14/conll14st-test-data/noalt/official-2014.combined.m2 | m2_to_src | reguligilo -a | pyspm_encode --model_file ../../../bpe/crawl19_16000/crawl19.16000.model > test.src
 
