3b8e92a97a15993258380b13f92b37afa7128185
nymwa <nymwa0@gmail.com>
2021-03-31 14:41:02 +0900

    add config

---
+++ data/alpha/bea19_valid/bea19_train_srctrg_16000/run.sh
@@ -0,0 +1,15 @@
+set -ex
+
+if [[ -z $WORKDIR ]] ; then
+    WORKDIR=`dirname $0`
+fi
+cd $WORKDIR
+
+if [[ -n $IS_SGE ]] ; then
+    . /etc/profile.d/modules.sh
+    source ../../../start.sh
+fi
+
+cat ../../../../corpora/bea19/train/wi+locness/wi+locness/m2/ABCN.dev.gold.bea19.m2 | m2_to_src | reguligilo -a | pyspm_encode --model_file ../../../../bpe/bea19_train_srctrg_16000/bea19_train_srctrg.16000.model > valid.src
+cat ../../../../corpora/bea19/train/wi+locness/wi+locness/m2/ABCN.dev.gold.bea19.m2 | m2_to_trg | reguligilo -a | pyspm_encode --model_file ../../../../bpe/bea19_train_srctrg_16000/bea19_train_srctrg.16000.model > valid.trg
+
